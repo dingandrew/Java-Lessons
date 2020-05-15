80b903bb3e32d76e65107c899619f9283b1e0f3e
Andrew Ding <a5ding@ucsd.edu>
2020-05-14 21:07:22 -0700

    exercies

---
+++ exercises/DebugMeSimple.java
@@ -0,0 +1,15 @@
+public class DebugMeSimple {
+    public static void main(String[] args) {
+
+        System.out.println(Need to print string. Am i missing something);
+
+        System.out.println("I am correct");
+
+        number = 0;
+        
+        if (numbr < 13) {
+            println(less than 13)
+        } else{
+            System.println("greater that 13")
+    }
+
